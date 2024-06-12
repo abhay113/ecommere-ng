a71acbb51d903e56ed3e0d98e71cb1cb1640c7ce
abhay <abhay@cateina.com>
2024-06-12 11:34:36 +0530

    add product form UI

--- src/app/components/seller-add-product/seller-add-product.component.ts
+++ src/app/components/seller-add-product/seller-add-product.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-seller-add-product',
   templateUrl: './seller-add-product.component.html',
-  styleUrl: './seller-add-product.component.scss'
+  styleUrl: './seller-add-product.component.scss',
 })
 export class SellerAddProductComponent {
-
+  submit(data: object) {
+    console.log(data);
+  }
 }
